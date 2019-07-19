fedac2586f3494da5caea75d6585e804c9e3c527
Hadrian Wegrzynowski <hadrian@hawski.com>
2019-07-19 15:28:15 +0200

    Implement simple test filter to be able to run all tests.

--- tests.c
+++ tests.c
@@ -5,16 +5,65 @@
 #include <unistd.h>
 
 #include "tests.h"
+#include "util.h"
 
-int __wrap_main(void)
+int
+skip_test(const char *name, int argc, char *argv[])
+{
+	int i;
+	if(argc == 1) {
+		return 0;
+	}
+	for(i = 1; i < argc; i++) {
+		if(!strcmp(argv[i], name)) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+static size_t run_tests_count = LEN(TESTS);
+
+static size_t
+next_from_all(int argc, char *argv[]) {
+	(void)argc; (void)argv;
+	static size_t i = 0;
+	return i++;
+}
+
+static size_t
+next_from_args(int argc, char *argv[])
+{
+	static int i = 1;
+	static size_t run = 0;
+
+	size_t j;
+
+	for(; i < argc; i++) {
+		for(j = 0; j < LEN(TESTS); j++) {
+			if(!strcmp(argv[i], TESTS[j].name)) {
+				i++;
+				run++;
+				return j;
+			}
+		}
+	}
+	run_tests_count = run;
+	return LEN(TESTS);
+}
+
+int __wrap_main(int argc, char *argv[])
 {
 	size_t i;
 	size_t success = 0;
-	const size_t len = sizeof(TESTS)/sizeof(TESTS[0]);
 	pid_t pid;
 	int status;
 
-	for(i = 0; i < len; i++) {
+	size_t (*next)(int, char*[]);
+
+	next = (argc > 1) ? next_from_args : next_from_all;
+
+	while((i = next(argc, argv)) < LEN(TESTS)) {
 		printf("Running: %s\n", TESTS[i].name);
 		pid = fork();
 		if(pid < 0) {
@@ -39,8 +88,8 @@ int __wrap_main(void)
 		}
 	}
 	printf("%s, tests ok: %zu / %zu\n",
-			success == len ? "success" : "failure",
-			success, len);
+			success == run_tests_count ? "success" : "failure",
+			success, run_tests_count);
 
-	return success == len ? 0 : 1;
+	return success == run_tests_count ? 0 : 1;
 }
